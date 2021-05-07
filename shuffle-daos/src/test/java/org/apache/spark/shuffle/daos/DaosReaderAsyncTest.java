c49868d3a8df112db0a47eefd3de5f0227fd18ba
jiafu zhang <jiafu.zhang@intel.com>
2021-05-07 18:40:30 +0800

    added license head
    Signed-off-by: jiafu zhang <jiafu.zhang@intel.com>

--- shuffle-daos/src/test/java/org/apache/spark/shuffle/daos/DaosReaderAsyncTest.java
+++ shuffle-daos/src/test/java/org/apache/spark/shuffle/daos/DaosReaderAsyncTest.java
@@ -1,3 +1,26 @@
+/*
+ * (C) Copyright 2018-2021 Intel Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * GOVERNMENT LICENSE RIGHTS-OPEN SOURCE SOFTWARE
+ * The Government's rights to use, modify, reproduce, release, perform, display,
+ * or disclose this software are subject to the terms of the Apache License as
+ * provided in Contract No. B609815.
+ * Any reproduction of computer software, computer software documentation, or
+ * portions thereof marked with this legend must also reproduce the markings.
+ */
+
 package org.apache.spark.shuffle.daos;
 
 import io.daos.DaosEventQueue;
@@ -5,7 +28,6 @@ import io.daos.obj.DaosObject;
 import io.daos.obj.IOSimpleDDAsync;
 import io.netty.buffer.ByteBuf;
 import org.apache.spark.SparkConf;
-import org.apache.spark.executor.ShuffleReadMetrics;
 import org.apache.spark.executor.TempShuffleReadMetrics;
 import org.apache.spark.shuffle.ShuffleReadMetricsReporter;
 import org.apache.spark.storage.BlockId;
